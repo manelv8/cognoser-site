633dfaed77ea02b8131b914fd9aff7ad476f3827
davi <manelv8@gmail.com>
2021-09-08 05:15:46 -0300

    basic project

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import '../styles/global.scss'
+import '../styles/style.scss'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
