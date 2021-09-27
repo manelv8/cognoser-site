09944d12d67a9e09ba67bfd67001c997784c4e54
davi <manelv8@gmail.com>
2021-09-27 17:42:34 -0300

    add testimonials

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,31 @@
 import '../styles/global.scss'
 import '../styles/style.scss'
+import { tns } from 'tiny-slider/src/tiny-slider.js';
 
 // document.querySelector('#app').innerHTML = `
 //   <h1>Hello Vite!</h1>
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `
+tns({
+  container: '.my-slider',
+  items: 3,
+  nav: false,
+  controls: false,
+  mouseDrag: true,
+  autoPlay: true,
+  slideBy: 'page',
+  autoplayTimeout: 300,
+  responsive: {
+    640: {
+      edgePadding: 20,
+      gutter: 20,
+      items: 2
+    },
+    700: {
+      gutter: 30
+    },
+    900: {
+      items: 3
+    }
+  }
+});
